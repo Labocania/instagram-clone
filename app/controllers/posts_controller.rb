04810c7a8b6607111ccc2382fd79c57d8c6aa3a1
hugokl <hugokl@outlook.com>
2015-10-28 06:09:06 +0000

    Added new action, installed simple_form gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,27 @@
 class PostsController < ApplicationController
   def index
   end
-
-  def create
+  
+  def new
+    @post = Post.new
   end
 
-  def read
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
   end
 
   def update
   end
 
-  def delete
+  def destroy
   end
+  
+  private
+    def post_params
+      params.require(:post).permit(:image, :caption)
+    end
 end
