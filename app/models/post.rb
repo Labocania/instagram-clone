2e22b440a38e30a5876007f674a6406b0864199c
hugokl <hugokl@outlook.com>
2015-10-29 06:19:15 +0000

    Implemented create post action with authentication, referenced user with post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   has_attached_file :image, styles: { :medium => "640x" }
   validates :image, presence: true
   validates :caption, presence: true
+  validates :user_id, presence: true
+  belongs_to :user
   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
