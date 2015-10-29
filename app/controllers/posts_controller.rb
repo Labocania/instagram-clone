63b5dd011176dbc8b183eca592da4df6636fc467
hugokl <hugokl@outlook.com>
2015-10-29 03:05:17 +0000

    Added edit and destroy actions and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
   def index
     @posts = Post.all
   end
@@ -13,17 +15,27 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+  
+  def edit
   end
 
   def update
+    @post.update(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
   
   private
     def post_params
       params.require(:post).permit(:image, :caption)
+    end
+    
+    def find_post
+      @post = Post.find(params[:id])
     end
 end
