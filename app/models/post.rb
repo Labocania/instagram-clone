ad5782d3dc1b0546b52431852c21adf150b5605e
hugokl <hugokl@outlook.com>
2015-11-04 06:24:32 +0000

    Added comment model, implemented on views

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   validates :caption, presence: true
   validates :user_id, presence: true
   belongs_to :user
+  has_many :comments, dependent: :destroy
   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
